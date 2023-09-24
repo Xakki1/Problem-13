4297b08f65e665034f48f9986d2cfac6ec6adf22
Раздольский Владислав <v.razdolskiy@mail.ru>
2023-09-25 00:05:40 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿int a, b;
+string s;
+Console.WriteLine("Введите номер карты = ");
+a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 6:
+        Console.WriteLine("Шестерка");   break;
+    case 7:
+        Console.WriteLine("Семерка ");   break;
+    case 8:
+        Console.WriteLine("Восьмерка "); break;
+    case 9:
+        Console.WriteLine("Девятка ");   break;
+    case 10:
+        Console.WriteLine("Десятка");    break;
+    case 11:
+        Console.WriteLine("Валет ");     break;
+    case 12:
+        Console.WriteLine("Дама ");      break;
+    case 13:
+        Console.WriteLine("Король ");    break;
+    case 14:
+        Console.WriteLine("Туз");        break;
+}
+Console.WriteLine("Введите номер масти = ");
+b = Convert.ToInt32(Console.ReadLine());
+switch (b)
+{
+    case 1:
+        Console.WriteLine(a + "Пики");  break;
+    case 2:
+        Console.WriteLine(" Трефы "); break;
+    case 3: 
+        Console.WriteLine(" Бубны "); break;
+    case 4:
+        Console.WriteLine(" Червы "); break;
+}
+Console.WriteLine();
+Console.ReadKey();
